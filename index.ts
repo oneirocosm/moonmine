125d423604bd2e173e5430619219181d49e72ed6
oneirocosm <software@oneirocosm.com>
2024-03-09 19:00:57 -0800

    feat: make bot invisible and invincible on launch
    This applies a couple commands that set the bot to creative mode, make
    it invisible, and move it up in the air.

--- index.ts
+++ index.ts
@@ -1,10 +1,20 @@
-process.env.DEBUG = 'minecraft-protocol';
+//process.env.DEBUG = 'minecraft-protocol';
 import mineflayer from "mineflayer";
+import prismarine from 'prismarine-viewer';
+const viewer = prismarine.mineflayer
 
 const mineBot = mineflayer.createBot({
     host: '127.0.0.1',
     username: 'bot',
     auth: 'offline',
+})
+
+mineBot.once('spawn', () => {
+    viewer(mineBot, { port: 3007, firstPerson: false }) // port is the minecraft server port, if first person is false, you get a bird's-eye view
+    mineBot.chat(`/gamemode creative ${mineBot.username}`);
+    mineBot.chat(`/effect give ${mineBot.username} minecraft:invisibility infinite 49 true`);
+    mineBot.chat(`/tp ${mineBot.username} 0 320 0`);
+    mineBot.creative.startFlying();
 })
 
 mineBot.on('chat', (username, message) => {
